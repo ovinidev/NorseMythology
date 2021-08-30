99258c97d2372b052ec99abb040cd1ee66709fdf
vinimedeiros13 <vinimedeiros7@outlook.com>
2021-08-30 18:09:10 -0300

    changing switcher

--- src/components/Switcher/index.jsx
+++ src/components/Switcher/index.jsx
@@ -2,17 +2,31 @@ import P from 'prop-types';
 
 import { useContext } from 'react';
 
+import Switch from 'react-switch';
+
 import { ThemeContext } from 'styled-components';
 import { Container, Input } from './styles';
 
 export const Switcher = ({ toggleTheme }) => {
-  const { title } = useContext(ThemeContext);
+  const { colors, title } = useContext(ThemeContext);
 
   return (
     <Container>
+      <Switch
+        class
+        onChange={toggleTheme}
+        checked={title === 'dark'}
+        checkedIcon={false}
+        uncheckedIcon={false}
+        height={10}
+        width={40}
+        handleDiameter={20}
+        offColor={colors.primaryColor}
+        onColor={colors.secondaryColor}
+      />
       {title === 'dark'
-        ? (<Input type="image" alt="image" src="https://image.flaticon.com/icons/png/512/979/979585.png" width="25" height="15" onClick={toggleTheme} />)
-        : (<Input type="image" alt="image" src="https://image.flaticon.com/icons/png/512/2949/2949009.png" width="25" height="15" onClick={toggleTheme} />)}
+        ? (<Input type="image" alt="moon" src="https://image.flaticon.com/icons/png/512/1152/1152393.png" onClick={toggleTheme} />)
+        : (<Input type="image" alt="sun" src="https://image.flaticon.com/icons/png/512/2907/2907217.png" onClick={toggleTheme} />)}
 
     </Container>
   );
--- src/components/Switcher/styles.js
+++ src/components/Switcher/styles.js
@@ -2,9 +2,13 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   color: ${({ theme }) => theme.colors.white} ;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
 
 export const Input = styled.input`
-  height: 30px;
-  width: 30px;
+  height: 35px;
+  width: 35px;
+  margin-left: 1.5rem;
 `;
